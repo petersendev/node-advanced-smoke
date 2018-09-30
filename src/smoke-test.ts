acc3e3e8e1aa0b661f1626d86d7343dd2d0ab41b
Arne Petersen <petersendev@users.noreply.github.com>
2018-09-30 12:17:43 +0200

    handle StatusCodeError

--- src/smoke-test.ts
+++ src/smoke-test.ts
@@ -9,22 +9,35 @@ export async function smokeTest(opts: ISmokeTestOptions, logger?: ISmokeTestLogg
 
     logger.log("testing url", opts.url);
 
-    let res = await request(opts.url, {
-        method: opts.method,
-        timeout: opts.timeout,
-        headers: opts.headers,
-        resolveWithFullResponse: opts.resolveWithFullResponse,
-        strictSSL: opts.strictSSL,
-        proxy: opts.proxy || process.env.ADVANCED_SMOKE_PROXY
-    });
+    let res: any;
+    try
+    {
+        res = await request(opts.url, {
+            method: opts.method,
+            timeout: opts.timeout,
+            headers: opts.headers,
+            resolveWithFullResponse: opts.resolveWithFullResponse
+        });
+    }
+    catch (e)
+    {
+        if (typeof e.statusCode !== "undefined")
+        {
+            res = e;
+        }
+        else
+        {
+            throw e;
+        }
+    }
 
     if (res.statusCode === opts.status)
     {
-        logger.log(`SUCCESS: received status code ${res.statusCode}, expected ${opts.status}`);
+        logger.log("statusCode", res.statusCode, ", expected", opts.status);
     }
     else
     {
-        logger.error(`FAILURE: received status code ${res.statusCode}, expected ${opts.status}`);
+        logger.error("statusCode", res.statusCode, ", expected", opts.status);
         return false;
     }
 
--- tests/smoke-test.mocked.spec.ts
+++ tests/smoke-test.mocked.spec.ts
@@ -29,9 +29,7 @@ describe("the smoke test", () =>
             method: "GET",
             resolveWithFullResponse: true,
             status: 200,
-            timeout: undefined,
-            strictSSL: true,
-            proxy: undefined
+            timeout: undefined
         };
 
         const res = await smokeTest(opts);
@@ -43,9 +41,7 @@ describe("the smoke test", () =>
             headers: opts.headers,
             method: opts.method,
             resolveWithFullResponse: opts.resolveWithFullResponse,
-            timeout: opts.timeout,
-            strictSSL: opts.strictSSL,
-            proxy: undefined
+            timeout: opts.timeout
         });
     });
 
@@ -57,9 +53,7 @@ describe("the smoke test", () =>
             method: "POST",
             resolveWithFullResponse: false,
             status: 200,
-            timeout: 500,
-            strictSSL: false,
-            proxy: "http://127.0.0.1:8888"
+            timeout: 500
         };
 
         const res = await smokeTest(opts);
@@ -71,9 +65,7 @@ describe("the smoke test", () =>
             headers: opts.headers,
             method: opts.method,
             resolveWithFullResponse: opts.resolveWithFullResponse,
-            timeout: opts.timeout,
-            strictSSL: opts.strictSSL,
-            proxy: opts.proxy
+            timeout: opts.timeout
         });
     });
 
@@ -85,42 +77,11 @@ describe("the smoke test", () =>
             method: "POST",
             resolveWithFullResponse: false,
             status: 400,
-            timeout: 500,
-            strictSSL: true,
-            proxy: undefined
+            timeout: 500
         };
 
         const res = await smokeTest(opts);
 
         expect(res).toBeFalsy();
-    });
-
-    it("should use ADVANCED_SMOKE_PROXY environment variable", async () =>
-    {
-        const opts: ISmokeTestOptions = {
-            url: "https://google.com",
-            headers: undefined,
-            method: "GET",
-            resolveWithFullResponse: true,
-            status: 200,
-            strictSSL: true,
-            timeout: undefined
-        };
-
-        process.env.ADVANCED_SMOKE_PROXY = "http://127.0.0.1:8888";
-
-        const res = await smokeTest(opts);
-
-        expect(res).toBeTruthy();
-
-        expect(mockRequest).toHaveBeenCalled();
-        expect(mockRequest).toHaveBeenCalledWith(opts.url, {
-            headers: opts.headers,
-            method: opts.method,
-            resolveWithFullResponse: opts.resolveWithFullResponse,
-            timeout: opts.timeout,
-            strictSSL: opts.strictSSL,
-            proxy: "http://127.0.0.1:8888"
-        });
     });
 });
